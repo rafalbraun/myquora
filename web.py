6387f90e112b67073e65bf60e1a256db28ed444e
rafalbraun <rafalbraun92@gmail.com>
2024-08-17 20:08:05 +0200

    works adding post

--- web.py
+++ web.py
@@ -19,7 +19,7 @@ create table if not exists POSTS (
 '''
 
 QUERY_SELECT_POST="select post_id, root_id, parent_id, content from posts where root_id = ?"
-QUERY_CREATE_POST=""
+QUERY_CREATE_POST="insert into posts(content) values(?)"
 QUERY_UPDATE_POST=""
 QUERY_DELETE_POST=""
 QUERY_SELECT_POSTS="select post_id, root_id, parent_id, content from posts"
@@ -29,8 +29,26 @@ def post(root_id):
 	with sqlite3.connect(dbname) as conn:
 		cursor = conn.cursor()
 		rows = cursor.execute(QUERY_SELECT_POST, (root_id,)).fetchall()
-		post = build_post_hierarchy(rows)[0]
-		return render_template("comments.html", post=post)
+		if len(rows) == 0:
+			return render_template("not_found.html", post_id=root_id)
+		result = build_post_hierarchy(rows)
+		if len(result) != 1:
+			return render_template("consistency_error.html", post_id=root_id)			
+		return render_template("post.html", post=result[0])
+
+@app.route("/post/create", methods=["GET","POST"])
+def post_create():
+	if request.method == 'GET':
+		return render_template("post_create.html")
+	if request.method == 'POST':
+		content = request.form.get('content')
+		## TODO validate content
+		with sqlite3.connect(dbname) as conn:
+			cursor = conn.cursor()
+			cursor.execute(QUERY_CREATE_POST, (content,))
+			lastrowid = cursor.lastrowid
+			cursor.execute("update posts set parent_id=?, root_id=? where post_id=?", (lastrowid,lastrowid,lastrowid))
+			return redirect(url_for(f'post', root_id=lastrowid))
 
 class Post:
 	def __init__(self, post_id, content):
@@ -54,19 +72,15 @@ class Post:
 def build_post_hierarchy(tuples):
 	posts = {}
 
-	# First pass: Create all Post objects and store them in a dictionary by post_id
 	for post_id, root_id, parent_id, content in tuples:
 		posts[post_id] = Post(post_id, content)
 
 	root_posts = []
 
-	# Second pass: Organize posts into the hierarchy
 	for post_id, root_id, parent_id, content in tuples:
 		if post_id == root_id == parent_id:
-			# This is a root post
 			root_posts.append(posts[post_id])
 		else:
-			# This is a child post, add it to the parent's replies
 			posts[parent_id].add_reply(posts[post_id])
 
 	return root_posts
