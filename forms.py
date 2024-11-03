7a7ae1fbebd06921b4fcfb8ea8d331da8d747764
rafalbraun <rafalbraun92@gmail.com>
2024-11-03 19:54:11 +0100

    prettier html template

--- forms.py
+++ forms.py
@@ -24,6 +24,6 @@ class CreateCommentForm(FlaskForm):
 
 class ReportPostForm(FlaskForm):
     id = IntegerField('', validators=[])
-    reason = RadioField('Label', choices=[('v1','violence'),('v2','spam'),('v3','obscenity'),('v4','other')])
+    reason = RadioField('report reason', choices=[('v1','violence'),('v2','spam'),('v3','obscenity'),('v4','other')])
     reported_post = IntegerField('', validators=[DataRequired()])
     submit2 = SubmitField('confirm')
